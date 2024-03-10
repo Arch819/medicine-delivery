ff107c53078c02510fef74dccfc84967a386dc01
ViacheslavArch <gabon.qwa@gmail.com>
2024-03-10 16:10:48 +0200

    delete unnecessary file and change serverURL

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import * as medicineApi from "./medicineApi";
 import * as orderApi from "./orderApi";
 import * as couponsApi from "./couponsApi";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://medicine-delivery-back-graa.onrender.com";
 
 const api = { shopApi, medicineApi, orderApi, couponsApi };
 export default api;
